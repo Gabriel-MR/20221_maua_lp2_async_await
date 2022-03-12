34ef6d970d9c0caecd7fefd8f92ad50fdc8526c2
Gabriel Moreira <gabss.moreira@icloud.com>
2022-03-12 13:03:41 -0300

    feat(async): rodando a api weather

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-//https://api.openweathermap.org/data/2.5/forecast?q=Itu&&appid=1c0f4f6d9368fdcfef13f735b0d92fdenpm
-
 require('dotenv').config()
-const exios = require ('axios')
+const { default: axios } = require('axios')
+const exios = require('axios')
 
 // const PROTOCOL = process.env.POTOCOL
 // const BASE_URL = process.env.BASE_URL
@@ -9,8 +8,32 @@ const exios = require ('axios')
 // const LANGUAGE = process.env.LANGUAGE
 // const UNITS = process.env.UNITS
 
-const {PROTOCOL, BASE_URL, APPID, LANGUAGE, UNITS, Q} = process.env
+const { PROTOCOL, BASE_URL, APPID, LANGUAGE, UNITS, Q } = process.env
 
 const url = `${PROTOCOL}://${BASE_URL}?appid=${APPID}&lang=${LANGUAGE}&units=${UNITS}&q=${Q}`
+// console.log(url)
 
-console.log(url)
+axios.get(url)
+    .then(res => {
+        console.log(res.data)
+        return res.data
+    })
+    .then(res => {
+        console.log(res.cnt)
+        return res
+    })
+    .then(res => {
+        return res['list']
+    })
+    .then(res => {
+
+    })
+    .then(res =>{
+        for (let previsao of res){
+            console.log(`${new Date(+previsao.dt*1000).toLocaleString()},
+            Min: ${previsao.main.temp_min}\u00B0C,
+            Max: ${previsao.main.temp_max}\u00B0C,
+            Hum: ${previsao.main.humidity}%,
+            ${previsao.weather[0].description}`)
+        }
+    })
